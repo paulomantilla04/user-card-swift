89a3537a528696b7ba2a5640784878454223a6d7
paulo <paulomantillaa@gmail.com>
2026-02-20 22:08:48 -0600

    Initial Commit

---
+++ userCard/userCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  userCardApp.swift
+//  userCard
+//
+//  Created by Paulo Mantilla on 20/02/26.
+//
+
+import SwiftUI
+
+@main
+struct userCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
